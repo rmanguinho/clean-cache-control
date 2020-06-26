33ed932eac7d70ee7b32d950c5519b0f423e1149
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-06-26 12:17:58 -0300

    add jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
