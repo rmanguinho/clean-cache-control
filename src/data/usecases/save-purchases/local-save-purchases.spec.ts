33d6a3126d04c22f99b9e1116cf79bfd1da75b2e
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-06-29 09:55:14 -0300

    merge tests

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -21,13 +21,6 @@ describe('LocalSavePurchases', () => {
     expect(cacheStore.messages).toEqual([])
   })
 
-  test('Should delete old cache on sut.save', async () => {
-    const { cacheStore, sut } = makeSut()
-    await sut.save(mockPurchases())
-    expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete, CacheStoreSpy.Message.insert])
-    expect(cacheStore.deleteKey).toBe('purchases')
-  })
-
   test('Should not insert new Cache if delete fails', async () => {
     const { cacheStore, sut } = makeSut()
     cacheStore.simulateDeleteError()
@@ -41,6 +34,7 @@ describe('LocalSavePurchases', () => {
     const purchases = mockPurchases()
     await sut.save(purchases)
     expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete, CacheStoreSpy.Message.insert])
+    expect(cacheStore.deleteKey).toBe('purchases')
     expect(cacheStore.insertKey).toBe('purchases')
     expect(cacheStore.insertValues).toEqual(purchases)
   })
