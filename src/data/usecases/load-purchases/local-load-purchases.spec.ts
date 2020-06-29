00282c4f281db36eed9e689f06dafdb7d63a8e76
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-06-29 14:34:44 -0300

    ensure LocalLoadPurchases returns am empty list if cache is 3 days old

--- src/data/usecases/load-purchases/local-load-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-load-purchases.spec.ts
@@ -62,4 +62,20 @@ describe('LocalLoadPurchases', () => {
     expect(cacheStore.deleteKey).toBe('purchases')
     expect(purchases).toEqual([])
   })
+
+  test('Should return an empty list if cache is 3 days old', async () => {
+    const currentDate = new Date()
+    const timestamp = new Date(currentDate)
+    timestamp.setDate(timestamp.getDate() - 3)
+    const { cacheStore, sut } = makeSut(currentDate)
+    cacheStore.fetchResult = {
+      timestamp,
+      value: mockPurchases()
+    }
+    const purchases = await sut.loadAll()
+    expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch, CacheStoreSpy.Action.delete])
+    expect(cacheStore.fetchKey).toBe('purchases')
+    expect(cacheStore.deleteKey).toBe('purchases')
+    expect(purchases).toEqual([])
+  })
 })
